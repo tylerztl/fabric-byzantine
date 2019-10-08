6448c878313479eb93f4d3a5f932c78c176b742c
zhangtailin <zhangtailin@zhigui.com>
2019-10-08 15:16:22 +0800

    update scripts

--- chaincode/src/token/token.go
+++ chaincode/src/token/token.go
@@ -10,10 +10,12 @@ import (
 )
 
 type Token struct {
-	Owner       string         `json:"Owner"`
-	TotalSupply int            `json:"TotalSupply"`
-	TokenName   string         `json:"TokenName"`
-	BalanceOf   map[string]int `json:"BalanceOf"`
+	Owner       string          `json:"Owner"`
+	TotalSupply int             `json:"TotalSupply"`
+	TokenName   string          `json:"TokenName"`
+	BalanceOf   map[string]int  `json:"BalanceOf"`
+	Peers       map[string]bool `json:"Peers"` // true: normal peer false: byzantine peer
+	Flag        bool            `json:"Flag"`
 }
 
 func (token *Token) transfer(_from string, _to string, _value int) {
@@ -28,17 +30,28 @@ func (token *Token) balance(_from string) int {
 }
 
 type Account struct {
-	Owner       string `json:"Owner"`
-	TokenName   string `json:"TokenName"`
-	TokenSymbol string `json:"TokenSymbol"`
-	Balance     int    `json:"BalanceOf"`
+	Owner     string `json:"Owner"`
+	TokenName string `json:"TokenName"`
+	Balance   int    `json:"BalanceOf"`
 }
 
 // Define the Smart Contract structure
 type TokenContract struct {
+	Peers map[string]bool
 }
 
 func (s *TokenContract) Init(stub shim.ChaincodeStubInterface) pb.Response {
+	s.Peers = make(map[string]bool)
+	s.Peers["peer0.org1.example.com"] = true
+	s.Peers["peer0.org2.example.com"] = true
+	s.Peers["peer0.org3.example.com"] = true
+	s.Peers["peer0.org4.example.com"] = true
+	s.Peers["peer0.org5.example.com"] = true
+	s.Peers["peer0.org6.example.com"] = true
+	s.Peers["peer0.org7.example.com"] = true
+	s.Peers["peer0.org8.example.com"] = true
+	s.Peers["peer0.org9.example.com"] = true
+	s.Peers["peer0.org10.example.com"] = true
 	return shim.Success(nil)
 }
 
@@ -56,6 +69,8 @@ func (s *TokenContract) issue(stub shim.ChaincodeStubInterface, args []string) p
 		TotalSupply: supply,
 		TokenName:   name,
 		BalanceOf:   map[string]int{},
+		Peers:       s.Peers,
+		Flag:        false, // true: byzantine peer attack
 	}
 
 	token.BalanceOf[token.Owner] = token.TotalSupply
@@ -69,9 +84,10 @@ func (s *TokenContract) issue(stub shim.ChaincodeStubInterface, args []string) p
 
 	return shim.Success(tokenAsBytes)
 }
+
 func (s *TokenContract) transfer(stub shim.ChaincodeStubInterface, args []string) pb.Response {
 
-	if len(args) != 4 {
+	if len(args) != 5 {
 		return shim.Error("Incorrect number of arguments. Expecting 4")
 	}
 	_from := args[1]
@@ -87,17 +103,21 @@ func (s *TokenContract) transfer(stub shim.ChaincodeStubInterface, args []string
 	}
 	fmt.Printf("transferToken - begin %s \n", string(tokenAsBytes))
 
+	flag, _ := strconv.ParseBool(args[4])
 	token := Token{
 		Owner:       "",
 		TotalSupply: 0,
 		TokenName:   "",
 		BalanceOf:   make(map[string]int),
+		Peers:       make(map[string]bool),
+		Flag:        false,
 	}
 
 	err = json.Unmarshal(tokenAsBytes, &token)
 	if err != nil {
 		return shim.Error(err.Error())
 	}
+	token.Flag = flag
 	token.transfer(_from, _to, _amount)
 
 	tokenAsBytes, err = json.Marshal(token)
@@ -109,6 +129,46 @@ func (s *TokenContract) transfer(stub shim.ChaincodeStubInterface, args []string
 		return shim.Error(err.Error())
 	}
 	fmt.Printf("transferToken - end %s \n", string(tokenAsBytes))
+
+	return shim.Success(tokenAsBytes)
+}
+
+func (s *TokenContract) setPeer(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	if len(args) != 3 {
+		return shim.Error("Incorrect number of arguments. Expecting 3")
+	}
+
+	tokenAsBytes, err := stub.GetState(args[0])
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	fmt.Printf("canch - begin %s \n", string(tokenAsBytes))
+
+	token := Token{
+		Owner:       "",
+		TotalSupply: 0,
+		TokenName:   "",
+		BalanceOf:   make(map[string]int),
+		Peers:       make(map[string]bool),
+		Flag:        false,
+	}
+
+	err = json.Unmarshal(tokenAsBytes, &token)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	token.Peers[args[1]], _ = strconv.ParseBool(args[2])
+
+	tokenAsBytes, err = json.Marshal(token)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	err = stub.PutState(args[0], tokenAsBytes)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	fmt.Printf("canch - end %s \n", string(tokenAsBytes))
 
 	return shim.Success(tokenAsBytes)
 }
@@ -134,9 +194,9 @@ func (s *TokenContract) balance(stub shim.ChaincodeStubInterface, args []string)
 	amount := token.balance(args[1])
 
 	account := Account{
-		Owner:       args[1],
-		TokenName:   token.TokenName,
-		Balance:     amount,
+		Owner:     args[1],
+		TokenName: token.TokenName,
+		Balance:   amount,
 	}
 	value := strconv.Itoa(amount)
 	tokenAsBytes, _ = json.Marshal(account)
@@ -155,6 +215,8 @@ func (s *TokenContract) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
 		return s.transfer(stub, args)
 	} else if function == "balance" {
 		return s.balance(stub, args)
+	} else if function == "setPeer" {
+		return s.setPeer(stub, args)
 	}
 
 	return shim.Error(fmt.Sprintf("Invalid Token Contract function name:%s", function))
