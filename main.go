03ea274b353f9fe0cc2c983a487288f3c66a42c1
zhangtailin <zhangtailin@zhigui.com>
2019-10-11 15:06:56 +0800

    refine listener

--- main.go
+++ main.go
@@ -4,19 +4,28 @@ import (
 	"fabric-byzantine/server"
 	"fabric-byzantine/server/helpers"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
 var logger = helpers.GetLogger()
 
+func timerTask() {
+	c := time.Tick(5 * time.Second)
+	for {
+		<-c
+		go server.GetSdkProvider().InvokeCC("mychannel1", "token", "transfer", [][]byte{[]byte("fab"), []byte("alice"), []byte("bob"), []byte("1"), []byte("true")})
+	}
+}
+
 func main() {
-	server.GetSdkProvider().BlockListener("mychannel1")
+	go server.GetSdkProvider().BlockListener("mychannel1")
+	go timerTask()
 
 	router := gin.Default()
-	router.GET("/query/:channel", func(c *gin.Context) {
-		channel := c.Param("channel")
-		data, err := server.GetSdkProvider().QueryCC(channel, "token", "balance", [][]byte{[]byte("fab"), []byte("alice")})
+	router.GET("/query", func(c *gin.Context) {
+		data, err := server.GetSdkProvider().QueryCC("mychannel1", "token", "balance", [][]byte{[]byte("fab"), []byte("alice")})
 		if err != nil {
 			logger.Error("query err: %v", err)
 			c.JSON(http.StatusOK, err)
